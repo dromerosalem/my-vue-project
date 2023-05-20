68f864ae1de385d73dbd2c6e7e44f4a7ba083fd2
David Romero <dromerosalem@gmail.com>
2023-05-20 13:45:01 +0100

    Weather app is now finish

--- README.md
+++ README.md
@@ -1,24 +1,32 @@
-# my-vue-app
+# My Weather Vue App
 
-## Project setup
-```
-npm install
-```
+This is a simple weather application built with Vue.js and Vuetify. It allows users to enter a city name and retrieve the current weather information for that city using an external API.
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
+## Technologies Used
 
-### Compiles and minifies for production
-```
-npm run build
-```
+- Vue.js: A progressive JavaScript framework for building user interfaces.
+- Vuetify: A Vue UI library that provides pre-built components following the Material Design guidelines.
+- Vuex: A state management pattern and library for Vue.js applications.
+- Axios: A popular HTTP client for making API requests.
 
-### Lints and fixes files
-```
-npm run lint
-```
+## Installation
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+1. Clone the repository: `git clone <repository_url>`
+2. Navigate to the project directory: `cd my-weather-vue-app`
+3. Install the dependencies: `npm install`
+
+## Usage
+
+1. Start the development server: `npm run serve`
+2. Open your web browser and visit: `http://localhost:8080` (or the URL shown in the console)
+3. Enter a city name in the input field and click "Get Weather" to retrieve the current weather information for that city.
+
+## Challenges and Achievements
+
+- Setting up Vue.js and Vuetify to be compatible was a challenging part of the development process. It required configuring the project correctly and ensuring the components were used properly.
+- The successful implementation of API calls and displaying the retrieved weather data was an achievement. It demonstrates the ability to integrate external APIs into a Vue.js application.
+
+## Important Note
+
+Please note that the external API currently in use may occasionally return a "503 Service Unavailable" error. In such cases, it is recommended to verify the API endpoint's availability using a tool like Postman. If the app is not working as expected, please check the API endpoint status.
+
--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,56 @@
 <template>
   <v-app id="inspire">
-    <v-app-bar
-      class="px-3"
-      color="white"
-      flat
-      density="compact"
-    >
-      <v-avatar
-        color="grey-darken-1"
-        size="32"
-      ></v-avatar>
+    <v-card class="text-center justify-center align-center">
+      <h1>My Weather Vue App</h1>
+    </v-card>
 
-      <v-spacer></v-spacer>
-
-      <v-tabs
-        centered
-        color="grey-darken-2"
-      >
-        <v-tab
-          v-for="link in links"
-          :key="link"
-        >
-          {{ link }}
-        </v-tab>
-      </v-tabs>
-      <v-spacer></v-spacer>
-
-      <v-avatar
-        class="hidden-sm-and-down"
-        color="grey-darken-1"
-        size="32"
-      ></v-avatar>
-    </v-app-bar>
-
-    <v-main style="background-color: lightblue;">
+    <v-main style="background-color: lightblue">
       <v-container>
         <v-row>
-          <v-col
-            cols="12"
-            sm="2"
-          >
+          <v-col cols="12" sm="2">
             <v-sheet
               rounded="lg"
               min-height="268"
+              class="text-center justify-center align-center marginTop"
             >
               <!--  -->
+              <div class="paddingTop fontSize">
+                <h5>Local Time: {{ localTime }}</h5>
+                <h5>Local Date: {{ localDate }}</h5>
+              </div>
             </v-sheet>
           </v-col>
 
-          <v-col
-            cols="12"
-            sm="8"
-          >
+          <v-col cols="12" sm="8">
             <v-sheet
-              min-height="70vh"
+              min-height="50vh"
               rounded="lg"
+              class="text-center justify-center align-center marginTop"
             >
               <!--  -->
-              <h1>City: {{city}}</h1>
-              <h3>Temperature: {{ data.temperature }}</h3>
-              <h3>Sky: {{ data.description }}</h3>
-              <h3>Wind: {{ data.wind }}</h3>
-
+              <div class="paddingTop fontSize">
+                <h1>City: {{ city }}</h1>
+                <h3>Temperature: {{ data.temperature }}</h3>
+                <h3>Sky: {{ data.description }}</h3>
+                <h3>Wind: {{ data.wind }}</h3>
+              </div>
             </v-sheet>
           </v-col>
 
-          <v-col
-            cols="12"
-            sm="2"
-          >
+          <v-col cols="12" sm="2">
             <v-sheet
               rounded="lg"
-              min-height="268"
+              min-height="150"
+              class="text-center justify-center align-center marginTop"
             >
               <!--  -->
-              <input v-model="newCity" type="text" placeholder="Enter city" />
-              <button @click="updateCity">Get Weather</button>
+
+              <v-text-field
+                v-model="newCity"
+                type="text"
+                label="City"
+              ></v-text-field>
+              <v-btn @click="updateCity">Get Weather</v-btn>
             </v-sheet>
           </v-col>
         </v-row>
@@ -85,52 +60,78 @@
 </template>
 
 <script>
-import{mapActions, mapState} from 'vuex';
-import store from './store'
+import { mapActions, mapState } from "vuex"
+import store from "./store"
 
-  export default {
-    data: () => ({
-      links: [
-        'Dashboard',
-        'Messages',
-        'Profile',
-        'Updates',
-      ],
-      city: 'Warsaw',
-      newCity: '',
-    }),
-    computed:{
-      ...mapState(['data'])
+export default {
+  data: () => ({
+    city: "Warsaw",
+    newCity: "",
+    localTime: "",
+    localDate: "",
+  }),
+  computed: {
+    ...mapState(["data"]),
+  },
+  methods: {
+    ...mapActions(["fetchData"]),
+    updateCity() {
+      this.city = this.newCity
+      this.fetchData(this.city)
+        .then((apiResponse) => {
+          console.log("APIResponse", JSON.parse(JSON.stringify(apiResponse)))
+          console.log("StoreData", JSON.parse(JSON.stringify(store.state.data)))
+        })
+        .catch((error) => {
+          console.error(error)
+        })
     },
-    methods:{
-      ...mapActions(['fetchData']),
-      updateCity(){
-        this.city = this.newCity;
-        this.fetchData(this.city)
-           .then((apiResponse) => {
-            console.log('APIResponse', JSON.parse(JSON.stringify(apiResponse)));
-            console.log('StoreData', JSON.parse(JSON.stringify(store.state.data)));
-             })
-    .catch((error) => {
-      console.error(error);
-     });
-      }
 
+    getLocalDateTime() {
+      const date = new Date()
 
+      // Get the local time
+      const hours = date.getHours()
+      const minutes = date.getMinutes()
+      const seconds = date.getSeconds()
+      this.localTime = `${hours}:${minutes}:${seconds}`
+
+      // Get the local date
+      const year = date.getFullYear()
+      const month = date.getMonth() + 1 // Month is zero-based, so add 1
+      const day = date.getDate()
+      this.localDate = `${year}-${month}-${day}`
     },
-    
-    
-    
-    mounted() {
-        this.fetchData(this.city)
-           .then((apiResponse) => {
-            console.log('APIResponse', JSON.parse(JSON.stringify(apiResponse)));
-            console.log('StoreData', JSON.parse(JSON.stringify(store.state.data)));
-             })
-    .catch((error) => {
-      console.error(error);
-     });
-    }
+  },
 
-  }
+  mounted() {
+    this.fetchData(this.city)
+      .then((apiResponse) => {
+        console.log("APIResponse", JSON.parse(JSON.stringify(apiResponse)))
+        console.log("StoreData", JSON.parse(JSON.stringify(store.state.data)))
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+
+    this.getLocalDateTime()
+  },
+}
 </script>
+
+<style>
+.custom-app-bar {
+  height: 48px; /* Adjust the height value as per your requirements */
+}
+.marginTop {
+  margin-top: 50px;
+}
+
+.paddingTop {
+  padding-top: 50px;
+}
+
+.fontSize {
+  font-size: 2em;
+}
+</style>
